11bb2c547274f420c5b1b6e00efd033d4d3fa07e
madkumar <iam.madhan2002@gmail.com>
2024-09-25 14:03:42 +0530

    Fetched categories from Api and displayed Dynamically

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,26 @@
+import {useEffect , useState} from 'react'
+
 const Header = () =>{
 
-    const categories = ["electronics" , 'jewelery' , 'mens clothing' , 'womens clothing']
+    const [categories,setCategories] = useState([])
+
+    // const categories = ["electronics" , 'jewelery' , 'mens clothing' , 'womens clothing']
+
+    useEffect(() => {
+        fetch(`https://fakestoreapi.com/products/categories`)
+            .then((res) => res.json())
+            .then((json) => {
+            setCategories(json);
+            // console.log(json);
+            });
+    }, []);
     
-    return <div className = 'header-items'> {
+    return (
+    <div className = 'header-items'> {
         categories.map((category) => (
             <div className='header-item'> {category} </div>
-        ))
-    }</div>
+        ))}
+    </div>)
 
 }
 
