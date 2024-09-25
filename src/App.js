b69d2b5597955d64db1589adce28d392541ae2b7
madkumar <iam.madhan2002@gmail.com>
2024-09-25 13:37:26 +0530

    Created Components Header and Product

--- src/App.js
+++ src/App.js
@@ -1,11 +1,15 @@
 
 import './App.css';
+import Header from './components/Header';
+import ProductList from './components/ProductList';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         Hello World
+        <Header/>
+        <ProductList/>
       </header>
     </div>
   );
